4886e4b56af161b4ffdc5b4d88329c735b2be771
Wil Cooley <wcooley@nakedape.cc>
2016-08-16 10:44:14 -0700

    Share test fixtures

--- tests/test_gryaml.py
+++ tests/test_gryaml.py
@@ -11,30 +11,6 @@ import gryaml.py2neo_compat
 from gryaml.py2neo_compat import Node, Relationship
 
 
-@pytest.fixture
-def graphdb():
-    """Fixture connecting to graphdb."""
-    if 'NEO4J_URI' not in os.environ:
-        pytest.skip('Need NEO4J_URI environment variable set')
-    graphdb = gryaml.connect(uri=os.environ['NEO4J_URI'])
-    graphdb.cypher.execute('MATCH (n) DETACH DELETE n')
-    return graphdb
-
-
-@pytest.yield_fixture
-def graphdb_offline():
-    """Ensure the database is not connected."""
-    neo4j_uri_env = os.environ.get('NEO4J_URI', None)
-    if neo4j_uri_env:
-        del os.environ['NEO4J_URI']
-    old_graphdb = gryaml.py2neo_compat.graphdb
-    gryaml.py2neo_compat.graphdb == None
-    yield
-    gryaml.py2neo_compat.graphdb = old_graphdb
-    if neo4j_uri_env:
-        os.environ['NEO4J_URI'] = neo4j_uri_env
-
-
 @pytest.mark.usefixtures('graphdb_offline')
 @pytest.mark.unit
 def test_node_parameter_permutation_offline():
